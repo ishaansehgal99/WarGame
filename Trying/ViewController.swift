06a34eec5a4e63d282eeb6cf3936a1d09a4760ca
ishaanforthewin <ishaanforthewin@gmail.com>
2016-07-10 17:22:34 -0500

    First mobile app. War card game.

--- Trying/ViewController.swift
+++ Trying/ViewController.swift
@@ -10,6 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    @IBOutlet weak var background: UIImageView!
+    @IBOutlet weak var playRoundButton: UIButton!
+    @IBOutlet weak var secondCardImageView: UIImageView!
+    @IBOutlet weak var firstCardImageView: UIImageView!
+    
+    @IBOutlet weak var playerScoreLabel: UILabel!
+    var playerScore:Int = 0
+    
+    @IBOutlet weak var enemyScoreLabel: UILabel!
+    var enemyScore:Int = 0
+    
+    var cardNamesArray:[String] = ["card1", "card2", "card3", "card4", "card5", "card6", "card7", "card8", "card9", "card10", "card11", "card12", "card13"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +34,37 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func playRoundTapped(sender: UIButton) {
+        
+        
+        let randomFirstNumber:Int = Int(arc4random_uniform(13))
+        
+        let firstNumberString:String = self.cardNamesArray[randomFirstNumber]
+        
+        self.firstCardImageView.image = UIImage(named: firstNumberString)
+        
+        let randomSecondNumber:Int = Int(arc4random_uniform(13))
+        
+        let secondNumberString:String = self.cardNamesArray[randomSecondNumber]
+        
+        self.secondCardImageView.image = UIImage(named: secondNumberString)
+        
+        if randomFirstNumber > randomSecondNumber{
+        //TODO: first card larger than second card
+            self.playerScore += 1;
+            self.playerScoreLabel.text = String(self.playerScore)
+        }
+        else if randomFirstNumber == randomSecondNumber{
+        //TODO: second card is equal to first card
+        
+        }
+        else {
+        //TODO: first card is smaller than second card
+            self.enemyScore += 1;
+            self.enemyScoreLabel.text = String(self.enemyScore)
+        
+        }
+    }
 
 }
 
